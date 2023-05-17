1fdf708d880a6200ec87084072d87111542fb521
semickolon <a9iqln3u@anonaddy.me>
2023-05-17 16:27:39 +0800

    Add Nix, direnv files

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "gomama";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = import nixpkgs { inherit system; };
+        gomama = pkgs.buildGoModule rec {
+          name = "gomama";
+          src = ./.;
+          vendorHash = "sha256-u+8StYUrSA6chPtbNzqYB5/o07PIYfZOzsw5Q7rqNho=";
+        };
+      in
+      {
+        packages.default = gomama;
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            go-outline
+            delve
+            go-tools
+          ];
+        };
+      }
+    );
+}
